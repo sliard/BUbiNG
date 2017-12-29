e0bb17692da7830b09c38fd8872ea81150b22f6a
Guillaume Pitel <guillaume.pitel@exensa.com>
2017-12-29 17:04:36 +0100

    Changed blacklist analysis to include locking + changed multi-level domain matching

--- src/it/unimi/di/law/bubing/frontier/BlackListing.java
+++ src/it/unimi/di/law/bubing/frontier/BlackListing.java
@@ -4,6 +4,9 @@ import com.google.common.primitives.Ints;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.net.URI;
+import java.util.concurrent.locks.Lock;
+
+import static crawlercommons.domains.PaidLevelDomain.getPLD;
 import static it.unimi.di.law.bubing.tool.HostHash.hostLongHash;
 
 public class BlackListing {
@@ -11,42 +14,44 @@ public class BlackListing {
 
     public static boolean checkBlacklistedIP(Frontier frontier, URI url, byte[] address)
     {
-        String host = url.getHost();
         boolean blackListed = false;
+        if (address.length != 4)
+            return false; // We check only IPv4 addresses
+        Lock lock = frontier.rc.blackListedHostHashesLock.readLock();
+        lock.lock();
 
         try {
             // We use Ints.fromBytes() as the array version generates an Object array just to log, possibly, a wrong argument (!)
             if (frontier.rc.blackListedIPv4Addresses.contains(Ints.fromBytes(address[0], address[1], address[2], address[3]))) {
                 if (LOGGER.isDebugEnabled()) LOGGER.debug("URL {} disallowed by last-minute check for IP blacklisting", url);
                 blackListed = true;
-                }
-            } catch (Exception e) {
-                LOGGER.warn("Exception in blacklist Ip checking {}", host, e);
             }
+        } catch (Exception e) {
+            LOGGER.warn("Exception in blacklist Ip checking {}", url, e);
+        } finally {
+            lock.unlock();
+        }
         return blackListed;
     }
 
     public static boolean checkBlacklistedHost(Frontier frontier, URI url)
     {
         String host = url.getHost();
+        String pld = getPLD(host);
         boolean blackListed = false;
-
+        Lock lock = frontier.rc.blackListedHostHashesLock.readLock();
+        lock.lock();
         try {
-            int firstPoint = 0;
-
-            do {
-                LOGGER.debug("Testing {} for blacklisting", host);
-                if (frontier.rc.blackListedHostHashes.contains(hostLongHash(host))) {
-                    if (LOGGER.isDebugEnabled())
-                        LOGGER.debug("URL {} disallowed by last-minute check for host blacklisting", url);
-                    blackListed = true;
-                }
-                firstPoint = host.indexOf('.') + 1; // firstPoint == 0 if not point found
-                if (firstPoint > 0)
-                    host = host.substring(firstPoint);
-            } while ((!blackListed) && (firstPoint > 0));
+               LOGGER.debug("Testing {} for blacklisting", pld);
+               if (frontier.rc.blackListedHostHashes.contains(hostLongHash(pld)) || frontier.rc.blackListedHostHashes.contains(hostLongHash(host))) {
+                   if (LOGGER.isDebugEnabled())
+                       LOGGER.debug("URL {} disallowed by last-minute check for Host blacklisting", url);
+                   blackListed = true;
+               }
         } catch (Exception e) {
-            LOGGER.warn("Exception in blacklist Host checking {}", host, e);
+            LOGGER.warn("Exception in blacklist Host checking {}", pld, e);
+        } finally {
+            lock.unlock();
         }
         return blackListed;
     }
