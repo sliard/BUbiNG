88e4d77dfada84ddd162e1c4f0e132206d054b58
Guillaume Pitel <guillaume.pitel@exensa.com>
2018-01-11 10:08:55 +0100

    Cleanup

--- test/it/unimi/di/law/bubing/util/IntCountMinSketchUnsafeTest.java
+++ test/it/unimi/di/law/bubing/util/IntCountMinSketchUnsafeTest.java
@@ -1,21 +1,26 @@
 package it.unimi.di.law.bubing.util;
 
 import com.google.common.primitives.Longs;
+import it.unimi.dsi.fastutil.io.BinIO;
 import jdk.nashorn.internal.ir.debug.ObjectSizeCalculator;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class IntCountMinSketchAtomicTest {
+public class IntCountMinSketchUnsafeTest {
 
     @Test
-    public void testSketch() {
+    public void testSketch() throws IOException, ClassNotFoundException {
         int s = (int) (1 * 1024 * 1024);
-        IntCountMinSketchAtomic cms = new IntCountMinSketchAtomic((int)s, 3);
-        ConcurrentCountingMap ccm = new ConcurrentCountingMap(1);
+        IntCountMinSketchUnsafe cms = new IntCountMinSketchUnsafe((int)s, 3);
 
         Random rng = new Random();
 
@@ -35,55 +40,12 @@ public class IntCountMinSketchAtomicTest {
             negdata[i] = v;
         }
 
-        System.out.println("Incrementing " + s + " items in counting maps with values 0 - " + maxVal);
-        System.gc();
-        double error = 0.0;
-        long count = 0;
-
-        long startCCMTime = System.nanoTime();
-        for (int p = 0; p < maxVal; p++)
-            count += indexes.parallelStream().mapToInt(i ->
-            {
-                ThreadLocalRandom trng = ThreadLocalRandom.current();
-                if (trng.nextDouble() < values[i]) {
-                    ccm.addTo(Longs.toByteArray(data[i]), 1);
-                    return 1;
-                } else return 0;
-            }).sum();
-        long endCCMTime = System.nanoTime();
-
-        System.out.println("CCM add total : " + (double)(endCCMTime-startCCMTime)/1000000000.0 + "s ; per increment " + (double)(endCCMTime-startCCMTime)/count);
-        System.out.println("Estimated CCM object size : " + ObjectSizeCalculator.getObjectSize(ccm));
-
-        long startGetCCMTime = System.nanoTime();
-        error = indexes.parallelStream().mapToDouble(i -> {
-                double e = maxVal * values[i] - ccm.get(Longs.toByteArray(data[i]));
-                return e * e;
-            }).sum();
-
-        System.gc();
-        long endGetCCMTime = System.nanoTime();
-        System.out.println("CCM Error get " + Math.sqrt(error)/s);
-        System.out.println("CCM get per item " + (double)(endGetCCMTime-startGetCCMTime)/s);
-
-        long startNegGetCCMTime = System.nanoTime();
-
-        error = indexes.parallelStream().mapToDouble(i -> {
-                    double e = ccm.get(Longs.toByteArray(negdata[i]));
-                    return e * e;
-            }).sum();
-        System.out.println("CCM Error get NEG " + Math.sqrt(error)/s);
-
-        System.gc();
-        long endNegGetCCMTime = System.nanoTime();
-        System.gc();
-        System.out.println("CCM neg get per item " + (double)(endNegGetCCMTime-startNegGetCCMTime)/s);
 
         // CMS
-        System.out.println("Incrementing " + s + " items in count min sketch with values 0 - " + maxVal);
+        System.out.println("Incrementing " + s + " items in count min sketch Unsafe with values 0 - " + maxVal);
 
         long startRHHSTime = System.nanoTime();
-        count = 0;
+        long count = 0;
         for (int p = 0; p < maxVal; p++)
             count += indexes.parallelStream().mapToInt(i ->
             {
@@ -96,25 +58,35 @@ public class IntCountMinSketchAtomicTest {
         long endRHHSTime = System.nanoTime();
         System.out.println("CMS add total : " + (double)(endRHHSTime-startRHHSTime)/1000000000.0 + "s ; per increment " + (double)(endRHHSTime-startRHHSTime)/count);
         System.out.println("Estimated CMS object size : " + ObjectSizeCalculator.getObjectSize(cms));
+
+        final File temp = File.createTempFile(ConcurrentCountingMap.class.getSimpleName() + "-", "-temp");
+        temp.deleteOnExit();
+        BinIO.storeObject(cms, temp);
+        final IntCountMinSketchUnsafe ncms = (IntCountMinSketchUnsafe) BinIO.loadObject(temp);
+
         long startGetRHHSTime = System.nanoTime();
-        error = indexes.parallelStream().mapToDouble(i -> {
-            double e = maxVal * values[i] - cms.get(Longs.toByteArray(data[i]));
+        double error = indexes.parallelStream().mapToDouble(i -> {
+            double e = maxVal * values[i] - ncms.get(Longs.toByteArray(data[i]));
             return e * e;
         }).sum();
 
         System.gc();
         long endGetRHHSTime = System.nanoTime();
         System.out.println("CMS Error get " + Math.sqrt(error)/s);
+        assertTrue(Math.sqrt(error)/s < 0.1);
         System.out.println("CMS get per item " + (double)(endGetRHHSTime-startGetRHHSTime)/s);
+        assert(error < 0.1);
 
         long startNegGetRHHSTime = System.nanoTime();
         error = indexes.parallelStream().mapToDouble(i -> {
-            double e = cms.get(Longs.toByteArray(negdata[i]));
+            double e = ncms.get(Longs.toByteArray(negdata[i]));
             return e* e;
         }).sum();
+        assertTrue(Math.sqrt(error)/s < 0.1);
         System.gc();
         long endNegGetRHHSTime = System.nanoTime();
         System.out.println("CMS Error get NEG " + Math.sqrt(error)/s);
         System.out.println("CMS neg get per item " + (double)(endNegGetRHHSTime-startNegGetRHHSTime)/s);
+
     }
 }
