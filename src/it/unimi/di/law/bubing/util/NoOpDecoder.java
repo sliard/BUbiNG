606fc1c5cf0818d476ec2aeffe4f2689e2119439
Guillaume Pitel <guillaume.pitel@exensa.com>
2018-01-26 22:07:07 +0100

    Added straight byte to char decoder for html parsing of non-modified byte stream, for extraction of non html parts for proper Charset detection

--- src/it/unimi/di/law/bubing/util/NoOpDecoder.java
+++ src/it/unimi/di/law/bubing/util/NoOpDecoder.java
@@ -1,4 +1,35 @@
 package it.unimi.di.law.bubing.util;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
 
-public class NoOpDecoder {
+
+public class NoOpDecoder extends CharsetDecoder
+{
+  public NoOpDecoder() {
+    super( Charset.defaultCharset(), 1.0f, 1.0f );
+  }
+
+
+  @Override
+  protected CoderResult decodeLoop( ByteBuffer in, CharBuffer out ) {
+    int pos = in.position();
+    try {
+      while ( in.hasRemaining() ) {
+        final byte b = in.get();
+        final char c = b >= 0 ? (char)b : (char)(b+256);
+        if ( !out.hasRemaining() )
+          return CoderResult.OVERFLOW;
+        out.put( c );
+        pos += 1;
+      }
+      return CoderResult.UNDERFLOW;
+    }
+    finally {
+      in.position( pos );
+    }
+  }
 }
+
