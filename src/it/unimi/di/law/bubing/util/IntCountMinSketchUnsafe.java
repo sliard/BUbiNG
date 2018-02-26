5ad93b62f13343ed3a130ffd100c34178e841281
Guillaume Pitel <guillaume.pitel@exensa.com>
2018-02-26 11:26:18 +0100

    added final for speed

--- src/it/unimi/di/law/bubing/util/IntCountMinSketchUnsafe.java
+++ src/it/unimi/di/law/bubing/util/IntCountMinSketchUnsafe.java
@@ -4,10 +4,10 @@ package it.unimi.di.law.bubing.util;
 
 import java.io.Serializable;
 
-public class IntCountMinSketchUnsafe implements Serializable, Cloneable{
-    private int depth;
-    private int width;
-    private int[][] arrays;
+public final class IntCountMinSketchUnsafe implements Serializable, Cloneable{
+    private final int depth;
+    private final int width;
+    private final int[][] arrays;
 
     public IntCountMinSketchUnsafe(int w, int d) {
         depth = d;
@@ -15,13 +15,12 @@ public class IntCountMinSketchUnsafe implements Serializable, Cloneable{
         arrays = new int[d][];
         for (int i=0; i<depth;i++)
             arrays[i] = new int[width];
-
     }
 
-    public int get(byte[] b) {
+    public final int get(final byte[] b) {
         return get(b, 0, b.length);
     }
-    public int get(byte[] b, int offset, int length) {
+    public final int get(final byte[] b, int offset, int length) {
         int val = Integer.MAX_VALUE;
         for (int i=0; i < depth; i++) {
             int c = arrays[i][(int) (MurmurHash3.hash(b, offset, length, i) & 0x7FFFFFFF) % width];
@@ -31,11 +30,11 @@ public class IntCountMinSketchUnsafe implements Serializable, Cloneable{
         return val;
     }
 
-    public void put(byte[] b, int value) {
+    public final void put(final byte[] b, final int value) {
         put(b, 0, b.length, value);
     }
 
-    public void put(byte[] b, int offset, int length, int value) {
+    public final void put(final byte[] b, final int offset, final int length, final int value) {
         for (int i=0; i < depth; i++) {
             int pos = (int) (MurmurHash3.hash(b, offset, length, i) & 0x7FFFFFFF) % width;
             if (arrays[i][pos] < value)
@@ -43,13 +42,13 @@ public class IntCountMinSketchUnsafe implements Serializable, Cloneable{
         }
     }
 
-    public int increment(byte[] b) {
+    public final int increment(final byte[] b) {
         return increment(b, 0, b.length);
     }
 
-    public int increment(byte[] b, int offset, int length) {
-        boolean updated = false;
-        int[] pos = new int[depth];
+    public final int increment(final byte[] b, final int offset, final int length) {
+
+        final int[] pos = new int[depth];
         for (int i = 0; i < depth; i++)
             pos[i] = (int) (MurmurHash3.hash(b, offset, length, i) & 0x7FFFFFFF) % width;
         int val = Integer.MAX_VALUE;
@@ -58,11 +57,14 @@ public class IntCountMinSketchUnsafe implements Serializable, Cloneable{
             if (c < val)
                 val = c;
         }
-        for (int i = 0; i < depth; i++) {
-            if (arrays[i][pos[i]] == val)
-                arrays[i][pos[i]] ++;
-        }
-        return val + 1;
+        if (val < Integer.MAX_VALUE) {
+            for (int i = 0; i < depth; i++) {
+                if (arrays[i][pos[i]] == val)
+                    arrays[i][pos[i]] = val+1;
+            }
+            return val + 1;
+        } else
+            return val;
     }
 
     private void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
