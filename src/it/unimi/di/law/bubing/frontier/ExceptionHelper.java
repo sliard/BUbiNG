e4223373832c4c3aa80785d600a957cbd220e569
emmanuel.marchand <emmanuel.marchand@exensa.com>
2020-02-21 10:45:42 +0100

    don't send ROBOTS.TXT fetchFailed

--- src/it/unimi/di/law/bubing/frontier/ExceptionHelper.java
+++ src/it/unimi/di/law/bubing/frontier/ExceptionHelper.java
@@ -1,8 +1,6 @@
 package it.unimi.di.law.bubing.frontier;
 
 import com.exensa.wdl.protobuf.crawler.EnumFetchStatus;
-import com.exensa.wdl.protobuf.crawler.MsgCrawler;
-import it.unimi.di.law.bubing.frontier.comm.FetchInfoSendThread;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2LongOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
@@ -19,7 +17,7 @@ public final class ExceptionHelper
   /** A map recording for each type of exception the number of retries. */
   public static final ObjectOpenHashSet<Class<?>> EXCEPTION_HOST_KILLER = new ObjectOpenHashSet<>();
   /** A map recording for each type of exception the FetchStatus */
-  public static final Object2IntOpenHashMap<Class<?>> EXCEPION_TO_FETCH_STATUS = new Object2IntOpenHashMap<>();
+  public static final Object2IntOpenHashMap<Class<?>> EXCEPTION_TO_FETCH_STATUS = new Object2IntOpenHashMap<>();
 
   static {
     EXCEPTION_TO_WAIT_TIME.defaultReturnValue( TimeUnit.HOURS.toMillis(1));
@@ -58,19 +56,19 @@ public final class ExceptionHelper
     EXCEPTION_HOST_KILLER.add(org.apache.http.conn.ConnectTimeoutException.class);
 
 
-    EXCEPION_TO_FETCH_STATUS.put(java.net.NoRouteToHostException.class, EnumFetchStatus.Enum.NO_ROUTE_TO_HOST_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(java.net.SocketException.class, EnumFetchStatus.Enum.SOCKET_ERROR_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(java.net.SocketTimeoutException.class, EnumFetchStatus.Enum.SOCKET_TIMEOUT_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(java.net.UnknownHostException.class, EnumFetchStatus.Enum.UNKNOWN_HOST_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(javax.net.ssl.SSLPeerUnverifiedException.class, EnumFetchStatus.Enum.SSL_ERROR_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.client.CircularRedirectException.class, EnumFetchStatus.Enum.CIRCULAR_REDIRECT_ERROR_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.client.RedirectException.class, EnumFetchStatus.Enum.REDIRECT_ERROR_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.conn.ConnectTimeoutException.class, EnumFetchStatus.Enum.HTTP_CONNECTION_TIMEOUT_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.ConnectionClosedException.class, EnumFetchStatus.Enum.HTTP_CONNECTION_CLOSED_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.conn.HttpHostConnectException.class, EnumFetchStatus.Enum.HTTP_HOST_CONNECT_ERROR_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.NoHttpResponseException.class, EnumFetchStatus.Enum.HTTP_NO_HTTP_RESPONSE_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.TruncatedChunkException.class, EnumFetchStatus.Enum.HTTP_TRUNCATED_CHUNK_VALUE);
-    EXCEPION_TO_FETCH_STATUS.put(org.apache.http.MalformedChunkCodingException.class, EnumFetchStatus.Enum.HTTP_MALFORMED_CHUNK_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(java.net.NoRouteToHostException.class, EnumFetchStatus.Enum.NO_ROUTE_TO_HOST_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(java.net.SocketException.class, EnumFetchStatus.Enum.SOCKET_ERROR_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(java.net.SocketTimeoutException.class, EnumFetchStatus.Enum.SOCKET_TIMEOUT_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(java.net.UnknownHostException.class, EnumFetchStatus.Enum.UNKNOWN_HOST_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(javax.net.ssl.SSLPeerUnverifiedException.class, EnumFetchStatus.Enum.SSL_ERROR_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.client.CircularRedirectException.class, EnumFetchStatus.Enum.CIRCULAR_REDIRECT_ERROR_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.client.RedirectException.class, EnumFetchStatus.Enum.REDIRECT_ERROR_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.conn.ConnectTimeoutException.class, EnumFetchStatus.Enum.HTTP_CONNECTION_TIMEOUT_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.ConnectionClosedException.class, EnumFetchStatus.Enum.HTTP_CONNECTION_CLOSED_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.conn.HttpHostConnectException.class, EnumFetchStatus.Enum.HTTP_HOST_CONNECT_ERROR_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.NoHttpResponseException.class, EnumFetchStatus.Enum.HTTP_NO_HTTP_RESPONSE_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.TruncatedChunkException.class, EnumFetchStatus.Enum.HTTP_TRUNCATED_CHUNK_VALUE);
+    EXCEPTION_TO_FETCH_STATUS.put(org.apache.http.MalformedChunkCodingException.class, EnumFetchStatus.Enum.HTTP_MALFORMED_CHUNK_VALUE);
 
   }
 }
