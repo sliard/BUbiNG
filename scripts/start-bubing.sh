f11e2db8af133ee5970651d4a10a4c29e4c99e2b
Geoffroy Fouquier <geoffroy.fouquier@exensa.com>
2020-08-26 13:26:22 +0200

    move priority logs with other logs

--- scripts/start-bubing.sh
+++ scripts/start-bubing.sh
@@ -16,22 +16,23 @@ NODE_ID=$( hostname | sed -e 's/[^0-9]//g' )
 NODE_ID=$(( $NODE_ID - 1 ))
 echo "node id = $NODE_ID"
 
+XTRA=""
 [[ -n $PRIORITY ]] && {
   KEEP_STORE=1
   AGENT_NAME=${AGENT_NAME}-PRIORITY
   XTRAOPT="-p -r ${DATA_DIR}"
+  XTRA="priority-"
 }
 
 # remove old crawling data
 [[ -z ${KEEP_STORE} ]] && rm -rf ${DATA_DIR}
 mkdir -p ${DATA_DIR}
 
-[[ -d ${LOG_DIR}/oldies ]] && rm -rf ${LOG_DIR}/oldies
-mkdir -p ${LOG_DIR}/oldies
-find ${LOG_DIR} -maxdepth 1 -name "bubing*" -exec mv {} ${LOG_DIR}/oldies/ \;
-
-[[ -f ${LOG_DIR}/console.log.err ]] && rm ${LOG_DIR}/console.log.err
-[[ -f ${LOG_DIR}/console.log.out ]] && rm ${LOG_DIR}/console.log.out
+[[ -d ${LOG_DIR}/oldies ]] || ${LOG_DIR}/oldies
+find ${LOG_DIR}/oldies -maxdepth 1 -name "${XTRA}bubing*" -delete
+find ${LOG_DIR}/oldies -maxdepth 1 -name "${XTRA}console.*" -delete
+find ${LOG_DIR} -maxdepth 1 -name "${XTRA}bubing*" -exec mv {} ${LOG_DIR}/oldies/ \;
+find ${LOG_DIR} -maxdepth 1 -name "${XTRA}console.*" -exec mv {} ${LOG_DIR}/oldies/ \;
 
 java -server -cp jars/"*":dependencies/"*":extjars/"*":. \
   -Xss256K -Xms${XMS} -Xmx${XMX} \
@@ -44,7 +45,8 @@ java -server -cp jars/"*":dependencies/"*":extjars/"*":. \
   -Dcom.sun.management.jmxremote.ssl=false \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -DBUBING_LOG_DIR=${LOG_DIR} \
+  -DBUBING_LOG_FILENAME=${XTRA}bubing \
   -javaagent:./jmx_prometheus_javaagent-0.3.1.jar=${PROMETHEUS_PORT}:jmxexporter.yml \
   it.unimi.di.law.bubing.Agent \
-  -h ${IP} -P ${PROPERTIES} -i ${NODE_ID} ${AGENT_NAME} -n $(echo ${XTRAOPT}) 2> ${LOG_DIR}/console.log.err > ${LOG_DIR}/console.log.out 
+  -h ${IP} -P ${PROPERTIES} -i ${NODE_ID} ${AGENT_NAME} -n $(echo ${XTRAOPT}) 2> ${LOG_DIR}/${XTRA}console.log.err > ${LOG_DIR}/${XTRA}console.log.out 
 
