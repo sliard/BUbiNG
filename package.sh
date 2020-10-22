71d86dead461a9dccb7dd9422bab8fc06ddf03eb
Geoffroy Fouquier <geoffroy.fouquier@exensa.com>
2020-10-22 16:13:46 +0200

    update package.sh to cope with no target dir

--- package.sh
+++ package.sh
@@ -10,15 +10,10 @@ BUBING_VERSION=$(grep "^version=" ${BUBING_SRC_DIR}/build.properties | cut -d '=
         exit 1
 }
 
-[[ -d target ]] || {
-  echo "ERROR: can't find target dir, compile (ant jar) before package"
-  exit 1
-}
-
 OUTPUT_DIR=target/${MODULE}
 
 [[ -d ${OUTPUT_DIR} ]] && rm -rf ${OUTPUT_DIR}
-mkdir ${OUTPUT_DIR}
+mkdir -p ${OUTPUT_DIR}
 cp -r scripts/* ${OUTPUT_DIR}/
 chmod u+x ${OUTPUT_DIR}/*.sh
 mkdir ${OUTPUT_DIR}/jars
